7181a613e41ee342a4f185d397a26c529fdb2d71
Ateeb Raza <ateebraza19.ar@gmail.com>
2019-02-05 01:14:22 +0530

    Updated Server.js

--- server/server.js
+++ server/server.js
@@ -1,44 +1,25 @@
-var mongoose= require('mongoose');
-mongoose.Promise=global.Promise;
+var express=require('express');
+var bodyParser=require('body-parser');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
 
-
-
-// // var newTodo=new Todo({
-// //     text:'Cook Dinner'
-// // });
-
-// // newTodo.save().then((doc)=>{
-// // console.log('Saved todo');
-// // console.log(doc); 
-// // },(e)=> {
-// //         console.log('Unable to save todo');
-// // });
-
-// var otherTodo=new Todo({
-//     text:'Edit'
-// });
-
-// otherTodo.save().then((doc)=>{
-//     console.log(JSON.stringify(doc,undefined,2));
-// },(e)=>{
-//     console.log('Unable to save OtherTodo');
-// });
-var User=mongoose.model('User',{
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1
-    }
+var app=express();
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+var todo=new Todo({
+    text:req.body.text
 });
-var User=new User({
-email:'ateeb@gmail.com'
-});
-User.save().then((doc)=>{
-    console.log(doc);
+todo.save().then((doc)=>{
+res.send(doc);
 },(e)=>{
-    console.log('Unable to connect');
-
+res.status(400).send(e);
 });
+});
+app.listen(3000,()=>{
+    console.log('Started on port 3000');
+})
+
+
+
